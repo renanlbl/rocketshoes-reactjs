3cf00a567bf79d6d65b51a68f0bd58ebce5efe6c
renanlimabl <renanlimabl@gmail.com>
2019-11-11 11:39:57 -0300

    Adiciona component header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
-  return (<h1>Hello World</h1>);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
